14119369f33fb134f8085cd41bf46fc160cb82ec
Stephan <stephan.s.p@outlook.com>
2024-02-06 16:18:22 +0100

    html: content, css: styling

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,3 +1,10 @@
-fetch('http://www.omdbapi.com/?apikey=[APIKEYHERE]&t=memento')
+fetch('http://www.omdbapi.com/?apikey=[]t=memento')
     .then((res) => res.json())
     .then((data) => console.log(data));
+    
+// async function getData() {
+//     const response = await fetch('http://www.omdbapi.com/?apikey=[]t=memento');
+//     return await response.json();
+// }
+
+// console.log(getData())
